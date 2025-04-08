cec77896683b32270bacfd0e7b20dd3c35cdac9a
Luddemannen20 <07ludlar@gmail.com>
2025-04-08 11:23:41 +0200

    08/04 - 25

--- pong1.rb
+++ pong1.rb
@@ -1,23 +1,53 @@
 require 'ruby2d'
 
-image = Image.new('img/background.jpg', width: 600, height: 600)
 set width: 600, height: 600
 
-#PONG_SOUND = Sound.new('')
-#PING_SOUND = Sound.new('')
+image = Image.new('img/background.jpg', width: 600, height: 600)
+PONG_SOUND = Sound.new('sound/pongsound.wav')
+PING_SOUND = Sound.new('sound/pingsound.wav')
+out_of_bounds = Sound.new('sound/outofbounds.mp3')
+@game_state = :start
 
-#class DividingLine
- # WIDTH = 15
- # HEIGHT = Window.height
- # NUMBER_OF_LINES = 1
+#Startskärm
+def draw_start_screen
+ start_image = Image.new('img/background.jpg', width: 600, height: 600)
+ start_image.draw
+  Text.new("WELCOME TO PONG!", 
+           x: (Window.width / 2) - 145,
+           y: 100, 
+           size: 30, 
+           color: 'black')
 
-  #def draw
-   # NUMBER_OF_LINES.times do |i|
-   #   Rectangle.new(x: (Window.width + WIDTH) / 2, y: (Window.height / NUMBER_OF_LINES) * i, height: HEIGHT, width: WIDTH, color: 'white')
-  #  end
- # end
-#end
+  @start_button = { x: (Window.width / 2) - 50, y: 250, width: 100, height: 40 }
+  Rectangle.new(
+    x: @start_button[:x], 
+    y: @start_button[:y], 
+    width: @start_button[:width], 
+    height: @start_button[:height], 
+    color: 'green'
+  )
+  Text.new("Start", 
+           x: @start_button[:x] + 20, 
+           y: @start_button[:y] + 8, 
+           size: 20, 
+           color: 'white')
+  
+  @quit_button = { x: (Window.width / 2) - 50, y: 320, width: 100, height: 40 }
+  Rectangle.new(
+    x: @quit_button[:x], 
+    y: @quit_button[:y], 
+    width: @quit_button[:width], 
+    height: @quit_button[:height], 
+    color: 'red'
+  )
+  Text.new("Quit", 
+           x: @quit_button[:x] + 25, 
+           y: @quit_button[:y] + 8, 
+           size: 20, 
+           color: 'white')
+end
 
+#Status i matchen
 class Scoreboard
   attr_reader :left_score, :right_score
 
@@ -103,8 +133,6 @@ class Paddle
   end
 end
 
-
-
 class Ball
   HEIGHT = 25
 
@@ -140,12 +168,11 @@ class Ball
   def move
     if hit_bottom?
       @y_velocity = -@y_velocity
-      #PONG_SOUND.play
+      PONG_SOUND.play
     elsif hit_top?
       @y_velocity = -@y_velocity
-      #PONG_SOUND.play
+      PONG_SOUND.play
     end
-    
     @x += @x_velocity
     @y += @y_velocity
   end
@@ -190,16 +217,14 @@ class Ball
   end
 end
 
-ball_velocity = 10 # Bollens hastighet
-
-player1 = Paddle.new(:left, 10)   # Ändra hastighet för spelare 1
-player2 = Paddle.new(:right, 10)  # Ändra hastighet för spelare 2
+ball_velocity = 10 
+player1 = Paddle.new(:left, 10)
+player2 = Paddle.new(:right, 10)
 @serving_player = :left
 scoreboard = Scoreboard.new
 spawn_y = (@serving_player == :left) ? (player1.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2) :
           (player2.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2)
 ball = Ball.new(ball_velocity, @serving_player, spawn_y)
-
 @game_paused = true
 @countdown_time = 3.0
 @last_time = Time.now
@@ -211,101 +236,123 @@ def start_countdown
 end
 
 update do
-  clear 
+  clear
 
-  image.draw
-  #DividingLine.new.draw
-
-  player1.move
-  player1.draw
-  player2.move
-  player2.draw
-
-  scoreboard.draw
-
-  if @game_paused
-    if @serving_player == :left
-      ball.instance_variable_set(:@x, player1.x + 25)
-      ball.instance_variable_set(:@y, player1.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2)
-    else
-      ball.instance_variable_set(:@x, player2.x - Ball::HEIGHT)
-      ball.instance_variable_set(:@y, player2.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2)
-    end
-    ball.draw
-    current_time = Time.now
-    elapsed = current_time - @last_time
-    @countdown_time = [@countdown_time - elapsed, 0].max
-    @last_time = current_time
-
-    countdown_display = @countdown_time.ceil
-    Text.new(countdown_display.to_s, x: (Window.width / 2) - 5, y: 2, size: 20, color: 'white')
-    
-    serve_text = @serving_player == :left ? "P1 serve" : "P2 serve"
-    Text.new(serve_text, x: (Window.width / 2) - 40, y: 20, size: 20, color: 'white')
-
-    if @countdown_time <= 0
-    end
-
-    next
-  end
+  case @game_state
+  when :start
+    draw_start_screen
   
-  if player1.hit_ball?(ball)
-    ball.bounce_off(player1)
-    #PING_SOUND.play
-  end
+  when :playing
+    image.draw
 
-  if player2.hit_ball?(ball)
-    ball.bounce_off(player2)
-    #PING_SOUND.play
-  end
+    player1.move
+    player1.draw
+    player2.move
+    player2.draw
 
-  ball.move
-  ball.draw
+    scoreboard.draw
 
-  if ball.out_of_bounds?
-    if ball.x <= 0
-      scoreboard.update(ball)
-      @serving_player = :right
-    elsif ball.shape.x2 >= Window.width
-      scoreboard.update(ball)
-      @serving_player = :left
+    if @game_paused
+      if @serving_player == :left
+        ball.instance_variable_set(:@x, player1.x + 25)
+        ball.instance_variable_set(:@y, player1.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2)
+      else
+        ball.instance_variable_set(:@x, player2.x - Ball::HEIGHT)
+        ball.instance_variable_set(:@y, player2.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2)
+      end
+      ball.draw
+      current_time = Time.now
+      elapsed = current_time - @last_time
+      @countdown_time = [@countdown_time - elapsed, 0].max
+      @last_time = current_time
+
+      countdown_display = @countdown_time.ceil
+      Text.new(countdown_display.to_s, x: (Window.width / 2) - 5, y: 2, size: 20, color: 'white')
+      
+      serve_text = @serving_player == :left ? "P1 serve" : "P2 serve"
+      Text.new(serve_text, x: (Window.width / 2) - 40, y: 20, size: 20, color: 'white')
+
+      if @countdown_time <= 0
+      end
+      
+      next
+    end
+    if player1.hit_ball?(ball)
+      ball.bounce_off(player1)
+      PING_SOUND.play
+    end
+    if player2.hit_ball?(ball)
+      ball.bounce_off(player2)
+      PING_SOUND.play
     end
 
-    spawn_y = if @serving_player == :left
-                player1.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2
-              else
-                player2.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2
-              end
-    ball = Ball.new(ball_velocity, @serving_player, spawn_y)
-    start_countdown
+    ball.move
+    ball.draw
+
+    if ball.out_of_bounds?
+      out_of_bounds.play
+      if ball.x <= 0
+        scoreboard.update(ball)
+        @serving_player = :right
+      elsif ball.shape.x2 >= Window.width
+        scoreboard.update(ball)
+        @serving_player = :left
+      end
+
+      spawn_y = if @serving_player == :left
+                  player1.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2
+                else
+                  player2.y + Paddle::HEIGHT / 2 - Ball::HEIGHT / 2
+                end
+      ball = Ball.new(ball_velocity, @serving_player, spawn_y)
+      start_countdown
+    end
   end
 end
 
-on :key_held do |event| # Spelare 1
-  if event.key == 'w'
-    player1.direction = :up
-  elsif event.key == 's'
-    player1.direction = :down
-  end
-end
+on :key_held do |event|
+  if @game_state == :playing
+    # Spelare 1
+    if event.key == 'w'
+      player1.direction = :up
+    elsif event.key == 's'
+      player1.direction = :down
+    end
 
-on :key_held do |event| # Spelare 2
-  if event.key == 'up'
-    player2.direction = :up
-  elsif event.key == 'down'
-    player2.direction = :down
+    # Spelare 2
+    if event.key == 'up'
+      player2.direction = :up
+    elsif event.key == 'down'
+      player2.direction = :down
+    end
   end
 end
 
 on :key_up do |_event|
-  player1.direction = nil
-  player2.direction = nil
+  if @game_state == :playing
+    player1.direction = nil
+    player2.direction = nil
+  end
 end
 
 on :key_down do |event|
-  if event.key == 'space' && @game_paused && @countdown_time <= 0
+  if @game_state == :playing && event.key == 'space' && @game_paused && @countdown_time <= 0
     ball.serve!
     @game_paused = false
+  end
+end
+
+on :mouse_down do |event|
+  if @game_state == :start
+    if event.x >= @start_button[:x] && event.x <= (@start_button[:x] + @start_button[:width]) &&
+       event.y >= @start_button[:y] && event.y <= (@start_button[:y] + @start_button[:height])
+      @game_state = :playing
+    end
+
+    if event.x >= @quit_button[:x] && event.x <= (@quit_button[:x] + @quit_button[:width]) &&
+       event.y >= @quit_button[:y] && event.y <= (@quit_button[:y] + @quit_button[:height])
+      close 
+    end
   end
 end
 
